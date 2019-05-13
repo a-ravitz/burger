d2b1fd43dc0e19ad024034c987527fd1e4dbe324
a-ravitz <adam.ravitz@gmail.com>
2019-05-13 15:56:52 -0400

    may 13.3

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ var mysql = require("mysql")
 
 var connection;
 
-if(process.env.JAWSDB_UTL) {
+if(process.env.JAWSDB_URL) {
 
-connection = mysql.createConnection(process.env.JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 
 } else {
 
